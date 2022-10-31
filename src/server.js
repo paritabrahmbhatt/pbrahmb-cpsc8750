2ece4f131e99418dbca39847f8c21609481311ed
Parita Brahmbhatt <67082863+paritabrahmbhatt@users.noreply.github.com>
2022-10-31 16:13:48 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,126 +1,93 @@
-// use the express library
 const express = require('express');
+const fetch = require('node-fetch');
 
-//import cookieParser
+const port = process.env.PORT || 3000;
 const cookieParser = require('cookie-parser');
-
-// create a new server application
 const app = express();
 
-// Define the port we will listen on
-// (it will attempt to read an environment global
-// first, that is for when this is used on the real
-// world wide web).
-const port = process.env.PORT || 3000;
-
 app.use(cookieParser());
-
-// set the view engine to ejs
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
-// The main page of our website
-//app.get('/', (req, res) => {
-  //res.send('Hello World!')
-//});
 
-/*app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <title>An Example Title</title>
-        <link rel="stylesheet" href="app.css">
-      </head>
-      <body>
-        <h1>Hello, World!</h1>
-        <p>HTML is so much better than a plain string!</p>
-      </body>
-    </html>
-  `);
-});*/
-
-/*app.get('/', (req, res) => {
-  // reads the url parameter
-  // http://domain/?name=text
-  const name = req.query.name || "World";
-
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <title>An Example Title</title>
-        <link rel="stylesheet" href="app.css">
-      </head>
-      <body>
-        <h1>Hello, ${name}!</h1>
-        <p>HTML is so much better than a plain string!</p>
-      </body>
-    </html>
-  `);
-});*/
-
-//const {encode} = require('html-entities');
-
-// ... snipped out code ...
-/*
-app.get('/', (req, res) => {
-  // reads the url parameter
-  // http://domain/?name=text
-  const name = req.query.name || "World";
-
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <title>An Example Title</title>
-        <link rel="stylesheet" href="app.css">
-      </head>
-      <body>
-        <h1>Hello, ${encode(name)}!</h1>
-        <p>HTML is so much better than a plain string!</p>
-      </body>
-    </html>
-  `);
-});
-
-app.get('/', (req, res) => {
-  res.render('welcome', {
-    name: req.query.name || "World",
-  });
-});*/
-
-//... code ...
 let nextVisitorId = 1;
-nextVisitorId += 1;
-
-//let currentTime = Date.now().toString();
-
-
+// The main page of our website
 app.get('/', (req, res) => {
-  let nVisitorId = req.cookies.visitorId ? nextVisitorId : ++nextVisitorId;
-  res.cookie('visitorId', nVisitorId);
-  res.cookie('visited', date_c =  Date.now().toString());
-  if (req.cookies.visited) {
-    req.cookies.visited = Math.floor((date_c - req.cookies.visited ) / 1000)
-  } else {
-    req.cookies.visited = null;
+  let hasVisited = false;
+  let visitorId = nextVisitorId;
+  if (req.cookies['visited']) {
+    hasVisited = true;
+    visitorId = req.cookies['visitorId'];
   }
-  console.log(req.cookies);
+  else{
+    nextVisitorId++;
+    hasVisited = false;
+  }
+
+  res.cookie('visitorId', visitorId);
+  res.cookie('visited', Date.now().toString());
+  
   res.render('welcome', {
     name: req.query.name || "World",
-    date: new Date().toLocaleString(),
-    timeofLastVisit: req.cookies.visited,
-    whoIsVisitor: nVisitorId,
+    datetime_now: new Date().toLocaleString(),
+    visitorID: visitorId,
+    lastvisit: Math.round(((new Date() - req.cookies['visited'])/1000)).toLocaleString(),
+    hasVisited: hasVisited,
+  },console.log(req.cookies));
 
-  });  
 });
 
+app.get("/trivia", async (req, res) => {
+  // fetch the data
+  const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
 
+  // fail if bad response
+  if (!response.ok) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+    return;
+  }
+
+  // interpret the body as json
+  const content = await response.json();
+
+  // fail if db failed
+  if (content.response_code !== 0) {
+    res.status(500);
+    res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+    return;
+  }
+
+  // respond to the browser
+  // TODO: make proper html
+  // res.send(JSON.stringify(content, 2));
+
+  const Triviaresults = content.results[0]
+  // console.log(Triviaresults)
+
+  var AnswersList = []
+  AnswersList.push(Triviaresults.correct_answer);
+  IncorrectAnswers = AnswersList.concat(Triviaresults.incorrect_answers);
+
+  let shuffled = IncorrectAnswers
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+
+  const answerAlert = shuffled.map(answer => {
+    return `<a href="javascript:alert('${
+      answer === Triviaresults.correct_answer ? 'Correct answer' : 'Incorrect answer'
+    }')">${answer}</a>`
+  })
+
+  res.render('trivia', {
+        question: Triviaresults.question,
+        category: Triviaresults.category,
+        difficulty: Triviaresults.difficulty,
+        answers: answerAlert
+    })
+});
 // Start listening for network connections
 app.listen(port);
 
